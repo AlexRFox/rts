882110a613fa6da5cb0e4ce126617963e79a0ad0
Alex Willisson <alex@anzat.lan>
2008-07-31 03:17:04 -0400

    updated to transfer to thud

--- rtsserver.c
+++ rtsserver.c
@@ -4,17 +4,37 @@
 #define PORT 9977
 #define PLAYERMAX 64
 
-int sock, port, mode;
+int sock, port, mode, numofplayers;
+char msg[1024];
 FILE *fp;
 
 struct player {
 	struct player *next;
 	struct sockaddr_in ca;
-	int playernum, joined;
+	double playernum, joined, lastpong;
 	char player_ip[20];
 };
 
-struct player *first_player, *last_player;
+struct packet {
+	char str[1024];
+	int playernum;
+};
+
+struct player *first_player, *last_player, *sending_player;
+
+struct player*
+idplayer (struct sockaddr_in *ca)
+{
+	struct player *pp;
+
+	for (pp = first_player; pp; pp = pp->next) {
+		if (pp->ca.sin_addr.s_addr == ca->sin_addr.s_addr) {
+			return (pp);
+		}
+	}
+
+	return (NULL);
+}
 
 void
 sendpacket (char str[1024], struct sockaddr_in *ca)
@@ -27,24 +47,24 @@ sendpacket (char str[1024], struct sockaddr_in *ca)
 }
 
 void
-sendtoall (char msg[1024])
+sendtoall (char str[1024])
 {
 	struct player *pp;
 
-	printf (msg);
+	printf (str);
 	for (pp = first_player; pp; pp = pp->next) {
-		sendpacket (msg, &(pp->ca));
+		sendpacket (str, &(pp->ca));
 	}
 }
 
 void
 createplayer (struct sockaddr_in *ca)
 {
-	char msg[1024];
-	int pn;
+	double pn, now;
 	struct player *pp, *pl;
 
 	pn = 0;
+	now = get_secs ();
 
 	pp = xcalloc (1, sizeof *pp);
 	if (first_player == NULL) {
@@ -57,12 +77,14 @@ createplayer (struct sockaddr_in *ca)
 					sprintf (msg, "ip already in use\n");
 					printf ("client failed to join, ip "
 						"already in use\n");
+					pl->lastpong = now;
 					sendpacket (msg, ca);
 					return;
 				} else {
-					pl->joined = 1;
-					sprintf (msg, "player %d joined\n",
+					sprintf (msg, "player %g joined\n",
 						pl->playernum);
+					pl->lastpong = now;
+					numofplayers++;
 					sendtoall (msg);
 					return;
 				}
@@ -74,10 +96,10 @@ createplayer (struct sockaddr_in *ca)
 	
 	last_player = pp;
 
-	pp->joined = 1;
 	pp->ca = *ca;
+	pp->lastpong = now;
 
-	sprintf (msg, "player %d joined\n", pp->playernum);
+	sprintf (msg, "player %g joined\n", pp->playernum);
 
 	sendtoall (msg);
 }
@@ -91,7 +113,8 @@ rmplayer (struct sockaddr_in *ca)
 	for (pp = first_player; pp; pp = pp->next) {
 		if (pp->ca.sin_addr.s_addr == ca->sin_addr.s_addr) {
 			pp->joined = 0;
-			sprintf (msg, "player %d has left\n", pp->playernum);
+			numofplayers--;
+			sprintf (msg, "player %g has left\n", pp->playernum);
 			sendtoall (msg);
 			return;
 		}
@@ -103,8 +126,11 @@ int
 main (int argc, char **argv)
 {
 	struct sockaddr_in sa, ca;
+	double now;
 
 	mode = 0;
+	numofplayers = 0;
+	now = get_secs ();
 
 	if (argc != 2) {
 		printf ("usage: rtsserver mapname.rtsmap\n");
@@ -113,9 +139,7 @@ main (int argc, char **argv)
 	char c, line[1024];
 	
 	fp = fopen (argv[1], "r");
-	
-	c = getc (fp);
-	
+		
 	sock = socket (PF_INET, SOCK_DGRAM, IPPROTO_IP);
 	
 	if (sock < 0) {
@@ -149,12 +173,33 @@ main (int argc, char **argv)
 				   (struct sockaddr*) &ca, &ca_len)) >= 0) {
 			buff[n] = 0;
 
+			if (numofplayers == 0) {
+				createplayer (&ca);
+				numofplayers++;
+			}
+
+			if ((sending_player = idplayer (&ca)) == NULL) {
+				createplayer (&ca);
+				if ((sending_player = idplayer (&ca))
+				    == NULL) {
+					printf ("unable to id player, "
+						"and/or failed to create\n");
+				}
+				numofplayers++;
+			}
+
 			switch (mode) {
 			case 0:
 				if (strcmp (buff, "join request\n") == 0) {
-					createplayer (&ca);
+					sending_player->joined = 1;
+					sprintf (msg, "player %g joined\n",
+						sending_player->playernum);
+					sendtoall (msg);
 				} else if (strcmp (buff, "leaving\n") == 0) {
-					rmplayer (&ca);
+					sending_player->joined = 0;
+					sprintf (msg, "player %g left\n",
+						sending_player->playernum);
+					sendtoall (msg);
 				} else if (strcmp (buff, "start game\n")
 					   == 0) {
 					mode = 1;
@@ -168,10 +213,12 @@ main (int argc, char **argv)
 			case 1:
 				if (strcmp (buff, "base map request\n")
 				    == 0 ) {
+					c = getc (fp);
+
 					while (c != EOF) {
 						ungetc (c, fp);
 						fgets (line, sizeof line, fp);
-						
+
 						msg[0] = 0;
 						
 						if (msg[0] == 0) {
@@ -184,7 +231,9 @@ main (int argc, char **argv)
 						}
 						c = getc (fp);
 					}
-					sendpacket (msg, &ca);
+					sendpacket (msg,
+						    &(sending_player->ca));
+					rewind (fp);
 				}
 				break;
 			}
